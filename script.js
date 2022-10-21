3518ffa31cd8fb7f97199548675bc2688d1719de
mariamnatroshvili3000 <112975366+mariamnatroshvili3000@users.noreply.github.com>
2022-10-21 19:45:28 +0400

    ჯან
    ჯანდაბა

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 var strSum = prompt ("შეიყვანეთ ანაბარის თანხა", 1000)
+var strPercent= prompt ("შეიყვანეთ საპროცენტო განაკვეთი", 10)
+
+var sum= parseInt (strSum);
+var percent = parseInt ( strPercent);
+
+sum=sum + sum * percent / 100;
+alert ( "პროცენტი დარიცხვის შემდეგ თანხა იქნება" + sum)
+
+
+else if (income >= 2000 && income <=4000) {
+    alert( "sashualo xelfasi")
+}
+
+var income = 1000
+if (income < 2000){
+    alert("საშუალოზე დაბალი")
+}
+else {
+    alert ("კაი ხელფასი")
+}
+
+var income = 200;
+switch (income) {
+    case 100:
+        console.log ("საშუალზე დაბალი")
+        break;
+    case 200 :
+        console.log ("საშუალო ხელფასი")
+        break
+    case 3000 :
+        console.log ("კაი ხელფასი")
+}
